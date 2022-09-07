6066d41fdc7109c75b59faf03f68e8d5e1b43e12
ybogdanov <79097146710@yandex.ru>
2022-09-07 20:24:27 +0400

    test

---
+++ src/test/java/meow/soft/socialnetwork/controllers/UserControllerTest.java
@@ -0,0 +1,125 @@
+package meow.soft.socialnetwork.controllers;
+
+import meow.soft.socialnetwork.model.User;
+import meow.soft.socialnetwork.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ExtendWith(SpringExtension.class)
+class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+
+    UUID userId = UUID.randomUUID();
+
+    UUID subscribeId = UUID.randomUUID();
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        when(userService.get(any())).thenReturn(new User());
+    }
+
+    @Test
+    void testAddSubscribe() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/user/addSubscribe/")
+                .param("userId", userId.toString())
+                .param("subscribeId", subscribeId.toString())
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+
+    }
+
+    @Test
+    void testRemoveSubscribe() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/user/removeSubscribe/")
+                .param("userId", userId.toString())
+                .param("subscribeId", subscribeId.toString())
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/user/")
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetOne() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/user/" + userId)
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        String content = "{\n" +
+                "    \"firstName\" : \"test2\"\n" +
+                "}";
+        mockMvc.perform(MockMvcRequestBuilders
+                .put("/api/user/")
+                .content(content)
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        String content = "{\n" +
+                "    \"firstName\" : \"test2\"\n" +
+                "}";
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/user/")
+                .content(content)
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/user/" + userId)
+                .contentType("application/json")
+        ).andExpect(status().isOk());
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
---
+++ src/test/java/meow/soft/socialnetwork/service/UserServiceTest.java
@@ -0,0 +1,88 @@
+package meow.soft.socialnetwork.service;
+
+import meow.soft.socialnetwork.model.User;
+import meow.soft.socialnetwork.repo.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+    @Mock
+    UserRepository userRepository;
+    @InjectMocks
+    UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGet() {
+        Optional<User> mock = Optional.of(mock(User.class));
+        when(userRepository.findById(any())).thenReturn(mock);
+
+        User result = userService.get(null);
+        Assertions.assertEquals(mock.get(), result);
+    }
+
+    @Test
+    void testAddSubscriber() {
+        Optional<User> mock = Optional.of(mock(User.class));
+        when(userRepository.findById(any())).thenReturn(mock);
+
+        userService.addSubscriber(null, null);
+    }
+
+    @Test
+    void testRemoveSubscriber() {
+        Optional<User> mock = Optional.of(mock(User.class));
+        when(userRepository.findById(any())).thenReturn(mock);
+
+        userService.removeSubscriber(null, null);
+    }
+
+    @Test
+    void testUpdate() {
+        Optional<User> mock = Optional.of(mock(User.class));
+        when(userRepository.findById(any())).thenReturn(mock);
+        when(userRepository.save(any())).thenReturn(mock.get());
+
+        User result = userService.update(mock.get());
+        Assertions.assertEquals(mock.get(), result);
+    }
+
+    @Test
+    void testCreate() {
+        User user = Mockito.mock(User.class);
+        when(userRepository.save(any())).thenReturn(user);
+        User result = userService.create(user);
+        Assertions.assertEquals(user, result);
+    }
+
+    @Test
+    void testDelete() {
+        Optional<User> mock = Optional.of(mock(User.class));
+        when(userRepository.findById(any())).thenReturn(mock);
+
+        userService.delete(null);
+    }
+
+    @Test
+    void testGetPage() {
+        when(userRepository.findAll(any())).thenReturn(null);
+
+        Page<User> result = userService.getPage(null);
+        Assertions.assertEquals(null, result);
+    }
+}
